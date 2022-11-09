f0ea84ee2e91c83555b5f52e5c8cd8c9f7bec6cc
Podi-Eka <podi.eka.online@gmail.com>
2022-11-09 22:38:48 +0530

    refactor

--- lib/features/interesting_areas/widgets/interesting_area_bubbles.dart
+++ lib/features/interesting_areas/widgets/interesting_area_bubbles.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 
 class InterestingAreaBubbles extends Forge2DGame {
   @override
-  Color backgroundColor() => Colors.blue;
+  Color backgroundColor() =>
+      Colors.blue; //add the colour that you need as background colour
 }
